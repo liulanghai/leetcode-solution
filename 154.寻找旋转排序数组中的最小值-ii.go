e445c753d2f6abdab393e1de1e7d520d227a4956
denghuaguang <denghuaguang@onething.net>
2019-09-20 14:29:36 +0800

    通过

---
+++ 154.寻找旋转排序数组中的最小值-ii.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=154 lang=golang
+ *
+ * [154] 寻找旋转排序数组中的最小值 II
+ */
+package cn
+
+func findMin2(nums []int) int {
+	l := len(nums)
+	start := 0
+	end := l - 1
+	for (start + 1) < end {
+		mid := (start + end) / 2
+		if nums[mid] > nums[end] { //在 mid - end 之间
+			start = mid
+		} else if nums[mid] == nums[end] {
+			end = end - 1
+		} else {
+			end = mid
+		}
+
+	}
+	if nums[start] > nums[end] {
+		return nums[end]
+	}
+	return nums[start]
+}
---
+++ 230.二叉搜索树中第k小的元素.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=golang
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// Definition for a binary tree node.
+
+package cn
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findK(root *TreeNode, count *int, k int, val *int) {
+	if k <= *count {
+		return
+	}
+	if root.Left != nil {
+		findK(root.Left, count, k, val)
+	}
+	*count = *count + 1
+	if *count == k {
+		*val = root.Val
+		return
+	}
+	if root.Right != nil {
+		findK(root.Right, count, k, val)
+	}
+}
+func kthSmallest(root *TreeNode, k int) int {
+	var val int
+	var count int
+	findK(root, &count, k, &val)
+	return val
+}
